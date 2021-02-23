40652ccb8bd92ffd7523aeccc5709134b016178e
ReineMoberg <reine.moberg3@gmail.com>
2021-02-23 11:35:33 +0100

    Changed ArrayList to HashSet instead in StudentDaoListImpl. Better choice in this case?
    Added spring configuration class ComponentScanConfig.

---
+++ src/main/java/se/lexicon/ReineMoberg/configs/ComponentScanConfig.java
@@ -0,0 +1,9 @@
+package se.lexicon.ReineMoberg.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "se.lexicon.ReineMoberg.*")
+public class ComponentScanConfig {
+}
